41951f8858348e526b78ce26580ac0bf50debe89
Brad Midgley <bmidgley@gmail.com>
2024-02-16 09:39:55 -0700

    erd

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # mars-evalink
+
+Make the erd:
+
+./manage.py graph_models -a -g -o ../docs/schema.png
--- evalink/evalink/settings.py
+++ evalink/evalink/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'evalink',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
